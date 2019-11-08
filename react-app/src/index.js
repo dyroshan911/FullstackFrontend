e97636ccca8e98872c8fd9700927857470d0cf65
yu_dai <yu_dai@cdmail.sonix.com.cn>
2019-11-08 14:25:02 +0800

    commit for eject

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM, { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
+import './static/less/todo.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let root = document.getElementById('root');
+
+render(<main className='pannel pannel-default'>
+
+</main>, root);
+
